bc1681cb2ce77a75ead242dfc5890a3a8c8f405d
cqlzx <bupt11lzx@163.com>
2016-11-25 12:57:55 -0500

    Finish registration part

--- app/src/main/java/com/yashtawade/foodforthought/activities/RegisterActivity.java
+++ app/src/main/java/com/yashtawade/foodforthought/activities/RegisterActivity.java
@@ -3,63 +3,120 @@ package com.yashtawade.foodforthought.activities;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.alibaba.fastjson.JSON;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
+import com.yashtawade.foodforthought.Http;
 import com.yashtawade.foodforthought.R;
+import com.yashtawade.foodforthought.constants.FFTConstant;
+import com.yashtawade.foodforthought.models.DataParse;
 
-/**
- * Created by yashtawade on 10/5/16.
- */
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+
+import cn.finalteam.okhttpfinal.BaseHttpRequestCallback;
+import cn.finalteam.okhttpfinal.RequestParams;
+import okhttp3.Headers;
+import okhttp3.Response;
 
 public class RegisterActivity extends Activity {
 
-    public void onRegisterClick(View view){
-        if(view.getId() == R.id.bSignUp)
-        {
-            EditText name = (EditText) findViewById(R.id.name);
-            EditText uname = (EditText) findViewById(R.id.username);
+    BaseHttpRequestCallback mCallback = new BaseHttpRequestCallback(){
+        @Override
+        public void onResponse(Response httpResponse, String response, Headers headers){
+            DataParse dp = JSON.parseObject(response, DataParse.class);
+            if(dp != null && dp.getError() != 1){
+                Intent i = MainActivity.newIntent(RegisterActivity.this, false);
+                startActivity(i);
+                Toast.makeText(RegisterActivity.this, "Congratulations! You're now a registered user!", Toast.LENGTH_SHORT).show();
+            }else if(dp == null){
+                Toast.makeText(RegisterActivity.this, "Error", Toast.LENGTH_SHORT).show();
+            }else{
+                //TODO exception type not specified
+                Toast.makeText(RegisterActivity.this, "Email has been registered", Toast.LENGTH_SHORT).show();
+            }
+        }
+    };
+
+    public void onRegisterClick(View view) {
+        if (view.getId() == R.id.bSignUp) {
+            EditText firstName = (EditText) findViewById(R.id.firstName);
+            EditText lastName = (EditText) findViewById(R.id.lastName);
             EditText email = (EditText) findViewById(R.id.email);
             EditText pass1 = (EditText) findViewById(R.id.pass1);
             EditText pass2 = (EditText) findViewById(R.id.pass2);
 
-            String namestr = name.getText().toString();
-            String unamestr = uname.getText().toString();
-            String emailstr = email.getText().toString();
+            String firstNameStr = firstName.getText().toString();
+            String lastNameStr = lastName.getText().toString();
+            String emailStr = email.getText().toString();
             String pass1str = pass1.getText().toString();
             String pass2str = pass2.getText().toString();
 
-            if (unamestr.isEmpty()){
-                Toast.makeText(RegisterActivity.this,"Please enter a valid username",Toast.LENGTH_SHORT).show();
-            }
-            if (emailstr.isEmpty()){
-                Toast.makeText(RegisterActivity.this,"Please enter a valid email address",Toast.LENGTH_SHORT).show();
-            }
-            if (namestr.isEmpty()){
-                Toast.makeText(RegisterActivity.this,"Please enter a valid name",Toast.LENGTH_SHORT).show();
-            }
+            if (firstNameStr.isEmpty()) {
+                Toast.makeText(RegisterActivity.this, "Please enter your first name", Toast.LENGTH_SHORT).show();
+            }else if (lastNameStr.isEmpty()) {
+                Toast.makeText(RegisterActivity.this, "Please enter your last name", Toast.LENGTH_SHORT).show();
+            }else if (!isValidEmailAddress(emailStr)) {
+                Toast.makeText(RegisterActivity.this, "Please enter a valid email address", Toast.LENGTH_SHORT).show();
+            }else if(pass1str.isEmpty()){
+                Toast.makeText(RegisterActivity.this, "Please enter your password", Toast.LENGTH_SHORT).show();
+            }else if(pass2str.isEmpty()){
+                Toast.makeText(RegisterActivity.this, "Please confirm your password", Toast.LENGTH_SHORT).show();
+            }else if (!pass1str.equals(pass2str)) {
+                Toast.makeText(RegisterActivity.this, "Passwords don't match!", Toast.LENGTH_SHORT).show();
+            }else{
+                MessageDigest md = null;
+                try {
+                    md = MessageDigest.getInstance("MD5");
+                } catch (NoSuchAlgorithmException e) {
+                    e.printStackTrace();
+                }
+                md.update(pass1str.getBytes());
 
-            if(!pass1str.equals(pass2str)) {
-                Toast.makeText(RegisterActivity.this,"Passwords don't match!",Toast.LENGTH_SHORT).show();
-            }
-            Toast.makeText(RegisterActivity.this,"Congratulations, you're now a registered user!",Toast.LENGTH_SHORT).show();
-            }
+                byte byteData[] = md.digest();
 
+                //convert the byte to hex format
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < byteData.length; i++) {
+                    sb.append(Integer.toString((byteData[i] & 0xff) + 0x100, 16).substring(1));
+                }
+                String password = sb.toString();
+
+                String url = FFTConstant.LONG_BASE_URL + "user/register";
+
+                RequestParams params = new RequestParams();
+                params.addFormDataPart("firstname", firstNameStr);
+                params.addFormDataPart("lastname", lastNameStr);
+                params.addFormDataPart("email", emailStr);
+                params.addFormDataPart("password", password);
+
+                Http httpRequest = new Http();
+                httpRequest.post(url, params, mCallback);
+            }
         }
 
-    public void takeBack(View view){
-        if(view.getId() == R.id.button){
-            Intent i = new Intent(getApplicationContext(),MainActivity.class);
-        startActivity(i);}
+    }
+
+    public void takeBack(View view) {
+        if (view.getId() == R.id.button) {
+            Intent i = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(i);
+        }
 
     }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.register);
+        setContentView(R.layout.activity_register);
         AdView adView = (AdView) findViewById(R.id.adView);
 
         AdRequest adRequest = new AdRequest.Builder().build();
@@ -67,5 +124,16 @@ public class RegisterActivity extends Activity {
         adView.loadAd(adRequest);
 
     }
+
+    private boolean isValidEmailAddress(String email){
+        boolean result = true;
+        try {
+            InternetAddress address = new InternetAddress(email);
+            address.validate();
+        } catch (AddressException e) {
+            result = false;
+        }
+        return result;
     }
+}
 
