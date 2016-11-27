ca1b7fb6baea124db2e1a6336bb211d49de4fd86
Yash Tawade <yashtawadeedu@gmail.com>
2016-11-27 16:25:48 -0600

    merge conflict

--- app/src/main/java/com/yashtawade/foodforthought/activities/WelcomeActivity.java
+++ app/src/main/java/com/yashtawade/foodforthought/activities/WelcomeActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.yashtawade.foodforthought.R;
 import com.yashtawade.foodforthought.Searchbyingri;
@@ -16,6 +17,7 @@ public class WelcomeActivity extends AppCompatActivity {
 
     Button search_recipe_welcome_button;
     Button missing;
+    ImageButton profile_image_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +32,15 @@ public class WelcomeActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+            }
+        });
+
+        profile_image_button = (ImageButton) findViewById(R.id.profile_image_button);
+        profile_image_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = ProfileActivity.newIntent(WelcomeActivity.this);
+                startActivity(i);
             }
         });
 
